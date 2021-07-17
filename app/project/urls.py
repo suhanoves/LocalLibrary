25f156dac769427bb6a33e12246e1b37c365ed0b
Eugene Suhanov <suhanoves@gmail.com>
2021-07-17 18:47:09 +0300

    🔗️ Add URL resolver for serving files in debug mode.

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,12 +1,21 @@
 """
 project URL Configuration
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+
+from project import settings
 
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='catalog:home', permanent=True)),
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Serving static files during development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serving files uploaded by a user during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
