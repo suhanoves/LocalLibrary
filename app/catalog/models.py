c2fed578142d3804d23c43bbcfc4071e0fd974b7
Eugene Suhanov <suhanoves@gmail.com>
2021-07-17 22:43:32 +0300

    🗃 Add basics models to the app

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -1,3 +1,118 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Book(models.Model):
+    """
+    Класс представляющий информацию о книге (не о конкретном экземпляре)
+    """
+    title = models.CharField(max_length=250)
+    author = models.ManyToManyField('Author')
+    summary = models.TextField(
+        max_length=1000,
+        help_text="Введите краткое описание книги"
+    )
+    imprint = models.ForeignKey('Imprint', on_delete=models.CASCADE)
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        help_text='13 символов <a href="https://www.isbn-international.org/content/what-isbn">ISBN</a>'
+    )
+    genre = models.ManyToManyField(
+        'Genre',
+        help_text='Выберите жанры, подходящие для этой книги'
+    )
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('catalog:book', kwargs={'id': self.id})
+
+
+class BookInstance(models.Model):
+    """
+    Модель, представляющая конкретный экземпляр книги
+    имеиющийся в нашей библиотеке и который можно взять
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="Уникальный идентификатор для этой конкретной книги во всей библиотеке"
+    )
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Тех.обслуживание'),
+        ('o', 'Арендована'),
+        ('a', 'Доступна'),
+        ('r', 'Зарезервирована'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        default='m',
+        help_text='Доступность книги'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} {self.book.title}'
+
+
+class Genre(models.Model):
+    """
+    Модель претсавляющая жанры книг (Научная фантастика, Проза)
+    """
+    name = models.CharField(
+        verbose_name='Тип',
+        max_length=200,
+        help_text='Укажите жанр книги: Фантастика, Французская поэзия и.т.д'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Language(models.Model):
+    """
+    Модель, представляющая основной язык книги
+    """
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    """
+    Модель, представляющая автора
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def ger_absolute_url(self):
+        return reverse('author', kwargs={'id': self.id})
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Imprint(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
